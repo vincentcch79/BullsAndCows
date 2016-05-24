b328c8f6a33e3aac4b1cd9b1666d4c03bfd20e1c
張智涵 <zhangzhihan@zhangzhihande-MacBook-Pro.local>
2016-05-24 16:00:55 +0800

    first commit

---
+++ BullsAndCows/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  BullsAndCows
+//
+//  Created by Brian Hu on 5/19/16.
+//  Copyright © 2016 Brian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var remainingTimeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var answearLabel: UILabel!
+    var remainingTime: UInt8! {
+        didSet {
+            remainingTimeLabel.text = "remaining time: \(remainingTime)"
+            if remainingTime == 0 {
+                guessButton.enabled = false
+            } else {
+                guessButton.enabled = true
+            }
+        }
+    }
+    
+    var hintArray = [(guess: String, hint: String)]() {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
+    // TODO: 1. decide the data type you want to use to store the answear
+    var answear: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setGame()
+    }
+    
+    func setGame() {
+        generateAnswear()
+        remainingTime = 9
+        hintArray.removeAll()
+        answearLabel.text = nil
+        guessTextField.text = nil
+    }
+    
+    func generateAnswear() {
+        var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        for _ in 0...3{
+            let n = Int(arc4random_uniform(UInt32(numArray.count)))
+            answear = answear + (numArray.removeAtIndex(n))
+        }
+        
+    }
+    // TODO: 2. generate your answear here
+    // You need to generate 4 random and non-repeating digits.
+    // Some hints: http://stackoverflow.com/q/24007129/938380
+    
+    
+    @IBAction func guess(sender: AnyObject) {
+        let guessString = guessTextField.text
+        guard guessString?.characters.count == 4 else {
+            let alert = UIAlertController(title: "you should input 4 digits to guess!", message: nil, preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        // TODO: 3. convert guessString to the data type you want to use and judge the guess
+        
+        guessString.array
+        
+        // TODO: 4. update the hint
+        
+        
+        
+        
+        let hint = "1A2B"
+        
+        hintArray.append((guessString!, hint))
+        
+        // TODO: 5. update the constant "correct" if the guess is correct
+        let correct = false
+        if correct {
+            let alert = UIAlertController(title: "Wow! You are awesome!", message: nil, preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            guessButton.enabled = false
+        } else {
+            remainingTime! -= 1
+        }
+    }
+    @IBAction func showAnswear(sender: AnyObject) {
+        // TODO: 6. convert your answear to string(if it's necessary) and display it
+        answearLabel.text = "\(answear)"
+    }
+    
+    @IBAction func playAgain(sender: AnyObject) {
+        setGame()
+    }
+    
+    // MARK: TableView
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return hintArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Hint Cell", forIndexPath: indexPath)
+        let (guess, hint) = hintArray[indexPath.row]
+        cell.textLabel?.text = "\(guess) => \(hint)"
+        return cell
+    }
+}
+
